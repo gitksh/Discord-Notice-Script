84416623e956527508aa33faec9cb068d253a7cd
KSH <40918958+gitksh@users.noreply.github.com>
2021-07-20 06:47:57 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+
+const config = {
+    'BOT_TOKEN': '',
+    'BOT_OWNER': '',
+    'NOTICE_TXT': ''
+};
+
