95b136ed234e486b8e3918802861997e974e5b45
KSH <40918958+gitksh@users.noreply.github.com>
2021-07-20 06:50:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ const config = {
 };
 
 client.login(config.BOT_TOKEN);
+client.login(config.BOT_TOKEN).then(async () => {
+    try {
+        const noticeTxt = await fs.readFileSync(config.NOTICE_TXT);
+
+        if (process.argv[2] !== 'release') {
+            let user = await client.users.fetch(config.BOT_OWNER, false, true);
+            await user.send(noticeTxt.toString());
+            console.log('DONE');
+        } else {
+            }
+        }
+    } catch (e) {
+        console.error(e);
+    }
+});
